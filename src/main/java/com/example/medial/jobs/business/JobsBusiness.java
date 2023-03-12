245d90e66fc39f70e46341becc939da746ca7c3c
GaezOrt <gaezort@gmail.com>
2023-03-12 14:02:14 -0300

    reinsert

--- src/main/java/com/example/medial/jobs/business/JobsBusiness.java
+++ src/main/java/com/example/medial/jobs/business/JobsBusiness.java
@@ -1,13 +1,19 @@
 package com.example.medial.jobs.business;
 
+import com.example.medial.jobs.models.Job;
 import com.example.medial.jobs.repositories.JobsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class JobsBusiness {
 
     @Autowired
-    private JobsRepository projectsRepository;
+    private JobsRepository jobsRepository;
 
+    public List<Job> recentJobs(){
+        return jobsRepository.findAll();
+    }
 }
--- src/main/java/com/example/medial/projects/controllers/ProjectController.java
+++ src/main/java/com/example/medial/projects/controllers/ProjectController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("newUserController")
+@RestController("projectsController")
 @RequestMapping("/1.0/projects")
 @CrossOrigin(origins = Api.API)
 @Controller
